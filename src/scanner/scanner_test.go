e52c65236db32dd000192b03cdfe12ae340ecc3d
Pergola <lolpsxvipkry2@gmail.com>
2021-11-17 23:36:44 -0300

    refactoring tests

--- src/scanner/scanner_test.go
+++ src/scanner/scanner_test.go
@@ -7,6 +7,52 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func scannerAsserts(t *testing.T, code string, nTokens, lines int) *Scanner {
+	s := Scanner{SourceCode: code}
+	s.ScanTokens()
+	assert.Len(t, s.Errors, 0, "should not have errors")
+	assert.Len(t, s.tokens, nTokens)
+	assert.Equal(t, lines, s.line)
+	return &s
+}
+
+func TestScanner_AddEOFToken(t *testing.T) {
+	s := scannerAsserts(t, "", 1, 1)
+	assert.Equal(t, s.tokens[0].Type, token.EOF_TT, "EOF should be added")
+}
+
+func TestScanner_AddOneRuneToken(t *testing.T) {
+	s := scannerAsserts(t, string(token.SEMICOLON_TT), 2, 1)
+	assert.Equal(t, token.SEMICOLON_TT, s.tokens[0].Type, "semicolon should have been added")
+}
+
+func TestScanner_AddOneOrTwoRuneToken(t *testing.T) {
+	s := scannerAsserts(t, string(token.GREATER_EQUAL_TT), 2, 1)
+	assert.Equal(t, token.GREATER_EQUAL_TT, s.tokens[0].Type, "greater_or_equal should have been added")
+}
+
+func TestScanner_IgnoresComments(t *testing.T) {
+	scannerAsserts(t, "// this is a coment!", 1, 1)
+}
+
+func TestScanner_IgnoresMeaningless(t *testing.T) {
+	scannerAsserts(t, "\t\r", 1, 1)
+}
+
+func TestScanner_AddNewLine(t *testing.T) {
+	scannerAsserts(t, "\n", 1, 2)
+}
+
+func TestScanner_ScanString(t *testing.T) {
+	s := scannerAsserts(t, "\"foobar\n\"", 2, 2)
+	assert.Equal(t, s.tokens[0].LiteralValue, "foobar\n")
+}
+
+func TestScanner_ScanEmptyString(t *testing.T) {
+	s := scannerAsserts(t, "\"\"", 2, 1)
+	assert.Equal(t, s.tokens[0].LiteralValue, "")
+}
+
 func TestScanner_InvalidCharacter(t *testing.T) {
 	invalidCharacter := "@"
 	scanner := Scanner{
@@ -16,7 +62,7 @@ func TestScanner_InvalidCharacter(t *testing.T) {
 	errors := scanner.Errors
 	assert.Len(t, errors, 1, "should have 1 error")
 	assert.Contains(t, errors[0].message, invalidCharacter)
-	assert.Contains(t, errors[0].message, "Unexpected character")
+	assert.Contains(t, errors[0].message, "unexpected character")
 }
 
 func TestScanner_InvalidCharacterAfterMatch(t *testing.T) {
@@ -28,97 +74,16 @@ func TestScanner_InvalidCharacterAfterMatch(t *testing.T) {
 	errors := scanner.Errors
 	assert.Len(t, errors, 1, "should have 1 error")
 	assert.Contains(t, errors[0].message, invalidCharacter)
-	assert.Contains(t, errors[0].message, "Unexpected character")
+	assert.Contains(t, errors[0].message, "unexpected character")
 }
 
-func TestScanner_AddEOFToken(t *testing.T) {
-	scanner := Scanner{}
-	scanner.ScanTokens()
-	errors := scanner.Errors
-	assert.Len(t, errors, 0, "should not have errors")
-	assert.Equal(t, scanner.tokens[0].Type, token.EOF_TT, "EOF should be added")
-}
-
-func TestScanner_AddOneRuneToken(t *testing.T) {
-	semiColon := string(token.SEMICOLON_TT)
+func TestScanner_UnterminatedString(t *testing.T) {
+	unterminatedString := "\"foobarz"
 	scanner := Scanner{
-		SourceCode: semiColon,
+		SourceCode: unterminatedString,
 	}
 	scanner.ScanTokens()
 	errors := scanner.Errors
-	assert.Len(t, errors, 0, "should not have errors")
-	assert.Equal(t, token.SEMICOLON_TT, scanner.tokens[0].Type, "semicolon should have been added")
-	assert.Len(t, scanner.tokens, 2, "should have semicolon and EOF")
-}
-
-func TestScanner_AddOneOrTwoRuneToken(t *testing.T) {
-	greaterOrEqual := string(token.GREATER_EQUAL_TT)
-	scanner := Scanner{
-		SourceCode: greaterOrEqual,
-	}
-	scanner.ScanTokens()
-	errors := scanner.Errors
-	assert.Len(t, errors, 0, "should not have errors")
-	assert.Equal(t, token.GREATER_EQUAL_TT, scanner.tokens[0].Type, "greater_or_equal should have been added")
-	assert.Len(t, scanner.tokens, 2, "should have greater_or_equal and EOF")
-}
-
-func TestScanner_IgnoresComments(t *testing.T) {
-	comments := "// this is a coment!"
-	scanner := Scanner{
-		SourceCode: comments,
-	}
-	scanner.ScanTokens()
-	errors := scanner.Errors
-	assert.Len(t, errors, 0, "should not have errors")
-	assert.Len(t, scanner.tokens, 1, "should have EOF only")
-}
-
-func TestScanner_IgnoresMeaningless(t *testing.T) {
-	whitespace := " \t\r"
-	scanner := Scanner{
-		SourceCode: whitespace,
-	}
-	scanner.ScanTokens()
-	errors := scanner.Errors
-	assert.Len(t, errors, 0, "should not have errors")
-	assert.Len(t, scanner.tokens, 1, "should have EOF only")
-}
-
-func TestScanner_AddNewLine(t *testing.T) {
-	whitespace := "\n"
-	scanner := Scanner{
-		SourceCode: whitespace,
-	}
-	scanner.ScanTokens()
-	errors := scanner.Errors
-	assert.Len(t, errors, 0, "should not have errors")
-	assert.Len(t, scanner.tokens, 1, "should have EOF only")
-	assert.Equal(t, 2, scanner.line, "should have added new line")
-}
-
-func TestScanner_ScanString(t *testing.T) {
-	stringCode := "\"foobar\n\""
-	scanner := Scanner{
-		SourceCode: stringCode,
-	}
-	scanner.ScanTokens()
-	errors := scanner.Errors
-	assert.Len(t, errors, 0, "should not have errors")
-	assert.Len(t, scanner.tokens, 2, "should have string + EOF")
-	assert.Equal(t, scanner.tokens[0].LiteralValue, "foobar\n")
-	assert.Equal(t, 2, scanner.line, "should have added new line")
-}
-
-func TestScanner_ScanEmptyString(t *testing.T) {
-	stringCode := "\"\""
-	scanner := Scanner{
-		SourceCode: stringCode,
-	}
-	scanner.ScanTokens()
-	errors := scanner.Errors
-	assert.Len(t, errors, 0, "should not have errors")
-	assert.Len(t, scanner.tokens, 2, "should have string + EOF")
-	assert.Equal(t, scanner.tokens[0].LiteralValue, "")
-	assert.Equal(t, 1, scanner.line, "should not have added new line")
+	assert.Len(t, errors, 1, "should have 1 error")
+	assert.Contains(t, errors[0].message, "unterminated string")
 }
